b4033239e7ec9dae1cb93b48a38d3ec1222824a4
Baral-Chief-of-Compliance <dartmol21@mail.ru>
2025-08-23 13:30:00 +0300

    add frontend

---
+++ frontend/kioskCZNfrontend/src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+
+          <q-icon name="warning" color="warning" size="4rem" />
+
+        </q-toolbar-title>
+        <main-logo color="red" />
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import MainLogo from 'src/components/MainLogo.vue';
+
+</script>
